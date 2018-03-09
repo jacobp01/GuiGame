c509c6ec216e17e048988857ec7e04f9e99f3aba
Jacob <34072284+jacobp01@users.noreply.github.com>
2018-03-09 11:04:52 -0800

    Background and Lad implemented

--- ballMoving.java
+++ ballMoving.java
@@ -1,12 +1,23 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.JFrame;
- 
+ import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.applet.*;
+import java.util.Vector;
+import java.util.Enumeration;
+import javax.imageio.*;
+import java.io.*;
+import javax.imageio.ImageIO;
 public class ballMoving extends Canvas {
- 
+ // starting pos
     int myX = 400;
     int myY = 400;
- 
+    Image background;
+    Image lad;
+
     public ballMoving() {
         setSize(new Dimension(500, 500));
         addKeyListener(new KeyAdapter() {
@@ -18,30 +29,48 @@ public class ballMoving extends Canvas {
     }
  
     public void paint(Graphics g) {
-        g.fillOval(myX, myY, 30, 30);
+          if(background == null){
+            try {
+                background = ImageIO.read(new File("C:/Users/fhsplab/Desktop/GuiGame/src/resources/desert.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Graphics og = background.getGraphics();
+        g.drawImage(background,0,0,this);
+        if( lad == null){
+            try {
+                lad = ImageIO.read(new File("C:/Users/fhsplab/Desktop/GuiGame/src/resources/lad.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Graphics mg = lad.getGraphics();
+        g.drawImage(lad,myX,myY,this);
+        // g.fillOval(myX, myY, 30, 30);
     }
- 
     public void moveIt(KeyEvent evt) {
      switch (evt.getKeyCode()) {
-            case KeyEvent.VK_DOWN:
-                myY += 5;
+            case KeyEvent.VK_S:
+                myY += 10;
                 break;
-            case KeyEvent.VK_UP:
-                myY -= 5;
+            case KeyEvent.VK_W:
+                myY -= 10;
                 break;
-            case KeyEvent.VK_LEFT:
-                myX -= 5;
+            case KeyEvent.VK_A:
+                myX -= 10;
                 break;
-            case KeyEvent.VK_RIGHT:
-                myX += 5;
+            case KeyEvent.VK_D:
+                myX += 10;
                 break;
         }
  
         repaint();
     }
- 
+    
     public static void main(String[] args) {
         JFrame frame = new JFrame("Basic Game");
+                System.out.print("Use WASD To Move");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ballMoving ex = new ballMoving();
         frame.getContentPane().add(ex);
