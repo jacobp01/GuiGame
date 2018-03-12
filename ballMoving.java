3a0c14b9887800d66535a0b0a88cf1928326400b
Jacob <34072284+jacobp01@users.noreply.github.com>
2018-03-12 11:05:29 -0700

    Monday Work

--- ballMoving.java
+++ ballMoving.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.*;
 import java.awt.event.*;
 import javax.swing.JFrame;
  import java.awt.event.ActionEvent;
@@ -11,16 +12,19 @@ import java.util.Enumeration;
 import javax.imageio.*;
 import java.io.*;
 import javax.imageio.ImageIO;
+import java.awt.List;
 public class ballMoving extends Canvas {
  // Plans:
  // Put in a side panel with items, just make another panel that's thin
  // make things happen when you press interact key when lad is in specific position
     
     // starting pos
-    int myX = 400;
-    int myY = 400;
+    int myX = 0;
+    int myY = 0;
+    int inventory = 0;
     Image background;
-    Image lad;
+    Image lad;  
+    ArrayList<String> inventoryString = new ArrayList<String>();
 
     public ballMoving() {
         setSize(new Dimension(500, 500));
@@ -33,6 +37,7 @@ public class ballMoving extends Canvas {
     }
  
     public void paint(Graphics g) {
+
           if(background == null){
             try {
                 background = ImageIO.read(new File("C:/Users/fhsplab/Desktop/GuiGame/src/resources/desert.png"));
@@ -56,25 +61,70 @@ public class ballMoving extends Canvas {
     public void moveIt(KeyEvent evt) {
      switch (evt.getKeyCode()) {
             case KeyEvent.VK_S:
-                myY += 10;
+                myY += 50;
                 break;
             case KeyEvent.VK_W:
-                myY -= 10;
+                myY -= 50;
                 break;
             case KeyEvent.VK_A:
-                myX -= 10;
+                myX -= 50;
                 break;
             case KeyEvent.VK_D:
-                myX += 10;
+                myX += 50;
+                break;
+            case KeyEvent.VK_E:
+                Interact();
+                break;
+            case KeyEvent.VK_I:
+                Inventory();
                 break;
         }
  
         repaint();
     }
-    
+    public void Interact(){
+       if(myX == 0 && myY == 0){
+           System.out.println("Your Trusty Sword lies at your feet. You pick it up.");
+           inventoryString.add("Trusty Sword");
+           inventory++;
+        }
+       else if(myX == 150 && myY == 150){
+           System.out.println("THE TEMPLE OF GROMBO THE FIFTH, ENTER IF YOU DARE");
+           //Grombo(evt);
+        }
+       else{
+       System.out.println("Nothing to interact with here"); 
+    }
+    }
+    public void Inventory(){
+        
+        if(inventory == 0){
+            System.out.println("You have no items in your inventory!");
+        }
+        else{
+            System.out.println("Your Inventory:");
+            for(String s : inventoryString){
+                System.out.println(s);
+            }
+        }
+    }
+    public void Grombo(KeyEvent evt){
+        System.out.println("Do you wish to enter Grombo's lair: Y/N");
+        switch (evt.getKeyCode()) {
+            case KeyEvent.VK_Y:
+                System.out.println("WORK IN PROGRESS");
+                break;
+            case KeyEvent.VK_N:
+                System.out.println("You leave the entrance to Grombo's lair, relief coursing through you.");
+                break;
+        }
+    }
     public static void main(String[] args) {
         JFrame frame = new JFrame("Basic Game");
-                System.out.print("Use WASD To Move");
+                System.out.println("Use WASD To Move and E to interact");
+                System.out.println("Press I to open your Inventory!");
+                System.out.println("");
+       
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ballMoving ex = new ballMoving();
         frame.getContentPane().add(ex);
